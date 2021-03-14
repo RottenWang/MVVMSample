44ae3e4590c5d16cce963d864c41c62ea1e10056
wangchen <wangchen@newrunway.com>
2021-03-14 19:43:52 +0800

    1

--- app/src/main/java/com/drwang/livedata/MainActivity.kt
+++ app/src/main/java/com/drwang/livedata/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.Observer
 import com.drwang.livedata.base.BaseMVVMActivity
 import com.drwang.livedata.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : BaseMVVMActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,16 @@ class MainActivity : BaseMVVMActivity() {
             }
         })
         viewModel.getName()
+        val client = OkHttpClient().newBuilder().build()
+        client.newCall(Request.Builder().build()).enqueue(object:Callback{
+            override fun onFailure(call: Call, e: IOException) {
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+            }
+        })
     }
+
 
 }
