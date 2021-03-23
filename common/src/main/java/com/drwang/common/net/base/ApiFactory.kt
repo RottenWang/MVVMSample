3b0dfc0b9266446cb09fae82b7752564e290eac6
wangchen <ccwz07@163.com>
2021-03-23 16:05:09 +0800

    删除无用

--- common/src/main/java/com/drwang/common/net/base/ApiFactory.kt
+++ common/src/main/java/com/drwang/common/net/base/ApiFactory.kt
@@ -1,6 +1,18 @@
 package com.drwang.common.net.base
 
-object ApiFactory : BaseApi() {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiFactory  {
+
+
+    var okHttpClient = OkHttpFactory.client
+    val mRetrofitBuilder: Retrofit.Builder by lazy {
+        Retrofit.Builder()
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(LiveDataCallAdapterFactory())
+    }
 
     private var myApi: MyApi? = null
 
